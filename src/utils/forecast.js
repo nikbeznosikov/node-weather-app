698c6408e76abebc16bd631d8a95aa961ac59e26
Nikita <nikitabeznosikov@gmail.com>
2020-01-01 22:04:22 -0800

    added aditional info about weather

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,15 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            console.log('body.daily.data[0]: ', body.daily.data[0]);
+            callback(
+                undefined,
+                `${body.daily.data[0].summary} 
+                It is currently ${body.currently.temperature} degress out. 
+                There is a ${body.currently.precipProbability}% chance of rain.
+                Max temperature ${body.daily.data[0].temperatureHigh} degress. 
+                Min temperature ${body.daily.data[0].temperatureLow} degrees`
+            )
         }
     })
 }
